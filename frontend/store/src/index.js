2acd6422792f5b5aabcc4f12e465ec60ee926a35
naol86 <naol.kasinet@a2sv.org>
2024-06-10 13:24:39 +0300

    done

--- frontend/store/src/index.js
+++ frontend/store/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from './App';
-import { Books } from "./compnonets";
+import { Books } from "./components";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
